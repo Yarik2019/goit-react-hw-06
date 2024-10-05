8944bc40a7fa36f1d55786a6c55150386de93f57
Yarik2019 <yroslavterno@gmail.com>
2024-10-05 22:54:00 +0300

    add redux

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,16 +1,29 @@
-import s from './ContactList.module.css';
-import Contact from '../Contact/Contact';
-const ContactList = ({ contacts, handleDeleteContact }) => {
+import s from "./ContactList.module.css";
+import Contact from "../Contact/Contact";
+import { useSelector } from "react-redux";
+import { selectContacts } from "../../redux/contactsSlice";
+import { selectNameFilter } from "../../redux/filtersSlice";
+const ContactList = () => {
+  const contacts = useSelector(selectContacts);
 
-    return (
-        <ul className={s.list}>
-            <li className={s.list}>
-                {contacts.map((contact) => (
-                    <Contact key={contact.id} {...contact} handleDeleteContact={handleDeleteContact} />
-                ))}
-            </li>
-        </ul>
-    )
-}
+  const filter = useSelector(selectNameFilter);
 
-export default ContactList
+  const filteredContacts = contacts.filter((contact) => {
+    return contact.name
+      .toLowerCase()
+      .trim()
+      .includes(filter.toLowerCase().trim());
+  });
+
+  return (
+    <ul className={s.list}>
+      <li className={s.list}>
+        {filteredContacts.map((contact) => (
+          <Contact key={contact.id} {...contact} />
+        ))}
+      </li>
+    </ul>
+  );
+};
+
+export default ContactList;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from "./components/App/App"
-import './index.css'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { createRoot } from "react-dom/client";
+import App from "./components/App/App";
+import "./index.css";
+import { Provider } from "react-redux";
+import { store, persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
+);
